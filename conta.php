e9f4d322742cf0389908dd8e5f7d4c76f6bc9b9e
unknown <ayodeleoduduwa52@gmail.com>
2019-04-10 06:42:14 -0300

    formulario

---
+++ conta.php
@@ -0,0 +1,8 @@
+<?php
+
+if(
+$a = $preco - ($preco*0.15)
+<?php echo($pag =="avista" ? : $valort = valor - ($Valor*0.15); ?>
+<?php echo($pag =="debito" ? : $valort = valor - ($Valor*0.10); ?>
+<?php echo($pag =="credito" ? : $valort = valor - ($Valor*0.5); ?>
+)
---
+++ pagamentophp.php
@@ -0,0 +1,11 @@
+<?php header("Content-type: text/html; charset=utf-8"); ?>
+<?php
+
+	
+	$preco = $_GET['preco'];
+	$parcela = $_GET['preco'] / $_GET['parcela'];
+	
+	print(preco);
+	?>
+	
+	
